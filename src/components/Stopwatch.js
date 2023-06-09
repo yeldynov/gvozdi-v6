9f7d2da6bd42a4ea57faa1d28a0e3b78e285eb48
yeldynov <yeldynov@gmail.com>
2023-06-09 16:51:35 +0300

    empty stopwatch

---
+++ src/components/Stopwatch.js
@@ -0,0 +1,11 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+
+const Stopwatch = () => {
+  return (
+    <View>
+      <Text>Stopwatch</Text>
+    </View>
+  );
+};
+
+export default Stopwatch;
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useContext } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  Image,
+  Dimensions,
+} from 'react-native';
+import Stopwatch from '../components/Stopwatch';
 
 const CreateScreen = () => {
   return (
-    <View>
-      <Text>CreateScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.image} source={require('../../assets/man.png')} />
+      <Text style={styles.createText}></Text>
+      <Stopwatch />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  createText: {
+    fontFamily: 'sans-serif-condensed',
+    textAlign: 'center',
+    marginHorizontal: 10,
+  },
+  title: {
+    alignSelf: 'center',
+    color: '#9B9B9B',
+    height: '50%',
+  },
+  container: {
+    flex: 1,
+  },
+  image: {
+    width: Dimensions.get('window').width - 100,
+    height: Dimensions.get('window').height / 2.2,
+    marginTop: 20,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+  },
+});
 
 export default CreateScreen;
